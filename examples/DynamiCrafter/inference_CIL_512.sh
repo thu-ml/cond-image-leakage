1b0b7a2aa23fb9660c77c63d3b2ad76a62200c6b
zhuhz22 <138758655+zhuhz22@users.noreply.github.com>
2024-06-26 17:50:00 +0800

    code

--- examples/DynamiCrafter/inference_CIL_512.sh
+++ examples/DynamiCrafter/inference_CIL_512.sh
@@ -12,7 +12,7 @@ res_dir="results" # file for outputs
 H=320
 W=512
 FS=24
-M=1000
+M=900
 
 CUDA_VISIBLE_DEVICES=1 python3 -m torch.distributed.launch \
 --nproc_per_node=1 --nnodes=1 --master_addr=127.0.0.1 --master_port=23459 --node_rank=0 \
