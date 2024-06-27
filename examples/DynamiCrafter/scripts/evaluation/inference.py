60dc035000f36a714bf147b87e4bf5897042471c
zhuhz22 <138758655+zhuhz22@users.noreply.github.com>
2024-06-27 17:56:12 +0800

    Update

--- examples/DynamiCrafter/scripts/evaluation/inference.py
+++ examples/DynamiCrafter/scripts/evaluation/inference.py
@@ -157,102 +157,8 @@ def get_latent_z(model, videos):
     z = rearrange(z, '(b t) c h w -> b c t h w', b=b, t=t)
     return z
 
-
-def image_guided_synthesis(model, prompts, videos, noise_shape, n_samples=1, ddim_steps=50, ddim_eta=1., \
-                        unconditional_guidance_scale=1.0, cfg_img=None, fs=None, text_input=False, multiple_cond_cfg=False, loop=False, interp=False, timestep_spacing='uniform_trailing', guidance_rescale=0.0,M=1000,**kwargs):
-    
-    
-    ddim_sampler = DDIMSampler(model) if not multiple_cond_cfg else DDIMSampler_multicond(model)
-    batch_size = noise_shape[0]
-    fs = torch.tensor([fs] * batch_size, dtype=torch.long, device=model.device)
-
-    if not text_input:
-        prompts = [""]*batch_size
-
-    img = videos[:,:,0] #bchw
-    img =img.to(model.device) #bchw
-    img_emb = model.embedder(img) ## blc
-    img_emb = model.image_proj_model(img_emb)
-
-    cond_emb = model.get_learned_conditioning(prompts)
-    cond = {"c_crossattn": [torch.cat([cond_emb,img_emb], dim=1)]}
-    if model.model.conditioning_key == 'hybrid':
-        z = get_latent_z(model, videos) # b c t h w
-        if loop or interp:
-            img_cat_cond = torch.zeros_like(z)
-            img_cat_cond[:,:,0,:,:] = z[:,:,0,:,:]
-            img_cat_cond[:,:,-1,:,:] = z[:,:,-1,:,:]
-        else:
-            img_cat_cond = z[:,:,:1,:,:]
-            img_cat_cond = repeat(img_cat_cond, 'b c t h w -> b c (repeat t) h w', repeat=z.shape[2])
-        cond["c_concat"] = [img_cat_cond] # b c 1 h w
-    
-    if unconditional_guidance_scale != 1.0:
-        if model.uncond_type == "empty_seq":
-            prompts = batch_size * [""]
-            uc_emb = model.get_learned_conditioning(prompts)
-        elif model.uncond_type == "zero_embed":
-            uc_emb = torch.zeros_like(cond_emb)
-        uc_img_emb = model.embedder(torch.zeros_like(img)) ## b l c
-        uc_img_emb = model.image_proj_model(uc_img_emb)
-        uc = {"c_crossattn": [torch.cat([uc_emb,uc_img_emb],dim=1)]}
-        if model.model.conditioning_key == 'hybrid':
-            uc["c_concat"] = [img_cat_cond]
-    else:
-        uc = None
-
-    ## we need one more unconditioning image=yes, text=""
-    if multiple_cond_cfg and cfg_img != 1.0:
-        uc_2 = {"c_crossattn": [torch.cat([uc_emb,img_emb],dim=1)]}
-        if model.model.conditioning_key == 'hybrid':
-            uc_2["c_concat"] = [img_cat_cond]
-        kwargs.update({"unconditional_conditioning_img_nonetext": uc_2})
-    else:
-        kwargs.update({"unconditional_conditioning_img_nonetext": None})
-
-    z0 = None
-    cond_mask = None
-
-    batch_variants = []
-    batch_variants_x_0_preds=[]
-    for _ in range(n_samples):
-
-        if z0 is not None:
-            cond_z0 = z0.clone()
-            kwargs.update({"clean_cond": True})
-        else:
-            cond_z0 = None
-        if ddim_sampler is not None:
-
-            samples, x_0_preds = ddim_sampler.sample(S=ddim_steps,
-                                            conditioning=cond,
-                                            batch_size=batch_size,
-                                            shape=noise_shape[1:],
-                                            verbose=False,
-                                            unconditional_guidance_scale=unconditional_guidance_scale,
-                                            unconditional_conditioning=uc,
-                                            eta=ddim_eta,
-                                            cfg_img=cfg_img, 
-                                            mask=cond_mask,
-                                            x0=cond_z0,
-                                            fs=fs,
-                                            timestep_spacing=timestep_spacing,
-                                            guidance_rescale=guidance_rescale,
-                                            ddpm_from=M,
-                                            **kwargs
-                                            )
-        ## reconstruct from latent to pixel space
-        batch_images = model.decode_first_stage(samples)
-        batch_variants.append(batch_images)
-        batch_x_0_pred=model.decode_first_stage(x_0_preds[1])
-        batch_variants_x_0_preds.append(batch_x_0_pred)
-    ## variants, batch, c, t, h, w
-    batch_variants = torch.stack(batch_variants)
-    batch_variants_x_0_preds=torch.stack(batch_variants_x_0_preds)
-    return batch_variants.permute(1, 0, 2, 3, 4, 5),batch_variants_x_0_preds.permute(1, 0, 2, 3, 4, 5)
-
 def image_guided_synthesis_analytic(model, prompts, videos, noise_shape, n_samples=1, ddim_steps=50, ddim_eta=1., \
-                        unconditional_guidance_scale=1.0, cfg_img=None, fs=None, text_input=False, multiple_cond_cfg=False, loop=False, interp=False, timestep_spacing='uniform_trailing', guidance_rescale=0.0,M=1000,analytic_init_path=None,**kwargs):
+                        unconditional_guidance_scale=1.0, cfg_img=None, fs=None, text_input=False, multiple_cond_cfg=False, loop=False, interp=False, timestep_spacing='uniform_trailing', guidance_rescale=0.0,M=1000,analytic_init_path=None,whether_analytic_init=1,**kwargs):
 
     dic=torch.load(analytic_init_path)
     expectation_X_0=dic["Expectation_X0"]
@@ -262,8 +168,11 @@ def image_guided_synthesis_analytic(model, prompts, videos, noise_shape, n_sampl
     alpha_t=sqrt_alpha_t**2
     sigma_p=torch.sqrt(1-alpha_t + alpha_t*tr_Cov_d)
     eps=torch.randn_like(mu_p)
-    init=mu_p+sigma_p*eps
     
+    if whether_analytic_init:
+        init=mu_p+sigma_p*eps
+    else :
+        init=torch.randn_like(mu_p)
     
     ddim_sampler = DDIMSampler(model) if not multiple_cond_cfg else DDIMSampler_multicond(model)
     batch_size = noise_shape[0]
@@ -415,14 +324,10 @@ def run_inference(args, gpu_num, gpu_no):
             
             
            
-            if args.whether_analytic_init==0:
-                batch_samples,_ = image_guided_synthesis(model, prompts, videos, noise_shape, args.n_samples, args.ddim_steps, args.ddim_eta, \
+
+            batch_samples = image_guided_synthesis_analytic(model, prompts, videos, noise_shape, args.n_samples, args.ddim_steps, args.ddim_eta, \
                             args.unconditional_guidance_scale, args.cfg_img, args.frame_stride, args.text_input, args.multiple_cond_cfg, args.loop, args.interp, args.timestep_spacing, args.guidance_rescale
-                            ,M=args.M)
-            else:
-                batch_samples = image_guided_synthesis_analytic(model, prompts, videos, noise_shape, args.n_samples, args.ddim_steps, args.ddim_eta, \
-                            args.unconditional_guidance_scale, args.cfg_img, args.frame_stride, args.text_input, args.multiple_cond_cfg, args.loop, args.interp, args.timestep_spacing, args.guidance_rescale
-                            ,M=args.M,analytic_init_path=args.analytic_init_path)
+                            ,M=args.M,analytic_init_path=args.analytic_init_path,whether_analytic_inti=args.whether_analytic_init)
             ## save each example individually
         
             for nn, samples in enumerate(batch_samples):
