1d671cc7980414838b2a094ec3d5451ca1de6411
zhuhz22 <zhuhongzhou>
2024-07-07 15:30:05 +0800

    refined training code  of SVD

--- examples/SVD/svd/training/loss.py
+++ examples/SVD/svd/training/loss.py
@@ -1,85 +1,28 @@
 import torch
-import torch.nn.functional as F
 from ..schedulers.edm import add_noise
 from ..models.utils import VScalingWithEDMcNoise
 from ..models.utils import pixel2latent
 import random
 
+# sample from a logit-normal distribution
 def logit_normal_sampler(m, s=1, beta_m=15, sample_num=1000000):
     y_samples = torch.randn(sample_num) * s + m
     x_samples = beta_m * (torch.exp(y_samples) / (1 + torch.exp(y_samples)))
     return x_samples
 
+# the $\mu(t)$ function
 def mu_t(t, a=5, mu_max=1):
     t = t.to('cpu')
     return 2 * mu_max * t**a - mu_max
     
-
-def get_sigma_s(t, a,beta_m):
+# get $\beta_s$ for TimeNoise
+def get_beta_s(t, a,beta_m):
     mu = mu_t(t,a=a)
     sigma_s = logit_normal_sampler(m=mu, sample_num=t.shape[0], beta_m=beta_m)
     return sigma_s
 
-class EDMLossDistribution:
-    def __init__(self, P_mean=-1.2, P_std=1.2, sigma_data=0.5,beta_m=15,mu_type='Convex_5'):
-        self.P_mean = P_mean
-        self.P_std = P_std
-        self.sigma_data = sigma_data
-        self.beta_m=beta_m
-        self.mu_type=mu_type
-        
-    def __call__(self, unet, latents, encoder_hidden_states,
-                 mixed_precision_training,pixel_values,vae,cfg_random_null_ratio,
-                 motion_bucket_id,fps):
-        noisy_latents, sigma = add_noise(latents, self.P_mean, self.P_std)
-
-        c_skip, c_out, c_in, c_noise = VScalingWithEDMcNoise(sigma)
-
-        scaled_inputs = noisy_latents * c_in
-        
-        # -------------------------------------------------------------------------------- #
-        
-        noise_aug_strength =get_sigma_s(sigma/700,self.mu_type,self.beta_m).reshape([latents.shape[0], 1, 1, 1, 1]).to(latents.device)
-        
-        
-        noisy_condition = pixel_values[:, 0, :, :, :].unsqueeze(dim=1)
-        rnd_normal = torch.randn([noisy_condition.shape[0], 1, 1, 1, 1], device=noisy_condition.device)
-        noisy_condition =noisy_condition + noise_aug_strength * rnd_normal
-        noisy_condition_latents = pixel2latent(noisy_condition, vae)/vae.config.scaling_factor
-        
-        # classifier-free guidance
-        if cfg_random_null_ratio > 0.0:
-            p = random.random()
-            noisy_condition_latents = noisy_condition_latents if p > cfg_random_null_ratio else torch.zeros_like(noisy_condition_latents)
-            encoder_hidden_states = encoder_hidden_states if p > cfg_random_null_ratio else torch.zeros_like(encoder_hidden_states)
-
-        # Repeat the condition latents for each frame so we can concatenate them with the noise
-        noisy_condition_latents = noisy_condition_latents.repeat(1, latents.shape[1], 1, 1, 1)
-        
-        # motion score
-        batch_size = noise_aug_strength.shape[0]
-            
-        motion_score = torch.tensor([motion_bucket_id]).repeat(batch_size).to(latents.device)
-        fps = torch.tensor([fps]).repeat(batch_size).to(latents.device)
-        added_time_ids = torch.stack([fps, motion_score, noise_aug_strength.reshape(batch_size)], dim=1)
-    
-        
-        
-        scaled_inputs = torch.cat([scaled_inputs,noisy_condition_latents], dim=2)
-        # ------------------------------------------------------------------------------ #
-        weight = (sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
-
-        with torch.cuda.amp.autocast(enabled=mixed_precision_training):
-            model_pred = unet(
-                scaled_inputs, c_noise, encoder_hidden_states, added_time_ids=added_time_ids)["sample"]
-
-            pred = model_pred * c_out + c_skip * noisy_latents
-            loss = torch.mean((weight.float() * (pred.float() - latents.float()) ** 2))
-
-        return loss
- 
-    
-class EDMLossDistributionLatent:
+# loss function for TimeNoise of the paper: https://arxiv.org/pdf/2406.15735.  
+class EDMLossTimeNoise:
     def __init__(self, P_mean=-1.2, P_std=1.2, sigma_data=0.5,beta_m=15,a=5):
         self.P_mean = P_mean
         self.P_std = P_std
@@ -90,21 +33,20 @@ class EDMLossDistributionLatent:
     def __call__(self, unet, latents, encoder_hidden_states,
                  mixed_precision_training,pixel_values,vae,cfg_random_null_ratio,
                  motion_bucket_id,fps):
+        # add noise to ground-truth video
         noisy_latents, sigma = add_noise(latents, self.P_mean, self.P_std)
-
         c_skip, c_out, c_in, c_noise = VScalingWithEDMcNoise(sigma)
-
         scaled_inputs = noisy_latents * c_in
         
-        # -------------------------------------------------------------------------------- #
+        # conditional image
+        img_condition = pixel_values[:, 0, :, :, :].unsqueeze(dim=1)
+        img_condition_latents = pixel2latent(img_condition, vae)/vae.config.scaling_factor
         
-        noise_aug_strength =get_sigma_s(sigma/700,self.a,self.beta_m).reshape([latents.shape[0], 1, 1, 1, 1]).to(latents.device)
+        # applying TimeNoise. Add logit-normal noise to the conditional image.
+        noise_aug_strength =get_beta_s(sigma/700,self.a,self.beta_m).reshape([latents.shape[0], 1, 1, 1, 1]).to(latents.device)
+        rnd_normal = torch.randn([img_condition_latents.shape[0], 1, 1, 1, 1], device=img_condition_latents.device)
+        noisy_condition_latents =img_condition_latents + noise_aug_strength * rnd_normal
         
-        
-        noisy_condition = pixel_values[:, 0, :, :, :].unsqueeze(dim=1)
-        noisy_condition_latents = pixel2latent(noisy_condition, vae)/vae.config.scaling_factor
-        rnd_normal = torch.randn([noisy_condition_latents.shape[0], 1, 1, 1, 1], device=noisy_condition_latents.device)
-        noisy_condition_latents =noisy_condition_latents + noise_aug_strength * rnd_normal
         # classifier-free guidance
         if cfg_random_null_ratio > 0.0:
             p = random.random()
@@ -114,19 +56,64 @@ class EDMLossDistributionLatent:
         # Repeat the condition latents for each frame so we can concatenate them with the noise
         noisy_condition_latents = noisy_condition_latents.repeat(1, latents.shape[1], 1, 1, 1)
         
-        # motion score
+
         batch_size = noise_aug_strength.shape[0]
-            
         motion_score = torch.tensor([motion_bucket_id]).repeat(batch_size).to(latents.device)
         fps = torch.tensor([fps]).repeat(batch_size).to(latents.device)
         added_time_ids = torch.stack([fps, motion_score, noise_aug_strength.reshape(batch_size)], dim=1)
     
-        
-        
         scaled_inputs = torch.cat([scaled_inputs,noisy_condition_latents], dim=2)
-        # ------------------------------------------------------------------------------ #
         weight = (sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
 
+        # calculate loss
+        with torch.cuda.amp.autocast(enabled=mixed_precision_training):
+            model_pred = unet(
+                scaled_inputs, c_noise, encoder_hidden_states, added_time_ids=added_time_ids)["sample"]
+
+            pred = model_pred * c_out + c_skip * noisy_latents
+            loss = torch.mean((weight.float() * (pred.float() - latents.float()) ** 2))
+
+        return loss
+    
+    
+
+# baseline loss function 
+class EDMLossBaseline:
+    def __init__(self, P_mean=-1.2, P_std=1.2, sigma_data=0.5):
+        self.P_mean = P_mean
+        self.P_std = P_std
+        self.sigma_data = sigma_data
+        
+    def __call__(self, unet, latents, encoder_hidden_states,
+                 mixed_precision_training,pixel_values,vae,cfg_random_null_ratio,
+                 motion_bucket_id,fps):
+        # add noise to ground-truth video
+        noisy_latents, sigma = add_noise(latents, self.P_mean, self.P_std)
+        c_skip, c_out, c_in, c_noise = VScalingWithEDMcNoise(sigma)
+        scaled_inputs = noisy_latents * c_in
+        
+        # baseline noise argumentation
+        noisy_condition, noise_aug_strength = add_noise(pixel_values[:, 0, :, :, :].unsqueeze(dim=1), P_mean=self.P_mean, P_std=self.P_std)
+        noisy_condition_latents = pixel2latent(noisy_condition, vae)/vae.config.scaling_factor
+
+        # classifier-free guidance
+        if cfg_random_null_ratio > 0.0:
+            p = random.random()
+            noisy_condition_latents = noisy_condition_latents if p > cfg_random_null_ratio else torch.zeros_like(noisy_condition_latents)
+            encoder_hidden_states = encoder_hidden_states if p > cfg_random_null_ratio else torch.zeros_like(encoder_hidden_states)
+
+        # Repeat the condition latents for each frame so we can concatenate them with the noise
+        noisy_condition_latents = noisy_condition_latents.repeat(1, latents.shape[1], 1, 1, 1)
+        
+        batch_size = noise_aug_strength.shape[0]
+        motion_score = torch.tensor([motion_bucket_id]).repeat(batch_size).to(latents.device)
+        fps = torch.tensor([fps]).repeat(batch_size).to(latents.device)
+        added_time_ids = torch.stack([fps, motion_score, noise_aug_strength.reshape(batch_size)], dim=1)
+    
+        scaled_inputs = torch.cat([scaled_inputs,noisy_condition_latents], dim=2)
+        weight = (sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
+
+        # calculate loss
         with torch.cuda.amp.autocast(enabled=mixed_precision_training):
             model_pred = unet(
                 scaled_inputs, c_noise, encoder_hidden_states, added_time_ids=added_time_ids)["sample"]
--- examples/SVD/train.py
+++ examples/SVD/train.py
@@ -1,11 +1,10 @@
 import os
-
 from torch.utils.tensorboard import SummaryWriter
-import random
 import logging
 import inspect
 import argparse
 import datetime
+from einops import rearrange
 from tqdm.auto import tqdm
 from omegaconf import OmegaConf
 from typing import Dict
@@ -16,25 +15,26 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from diffusers import AutoencoderKLTemporalDecoder
 from transformers import CLIPImageProcessor, CLIPVisionModelWithProjection
 from diffusers.training_utils import EMAModel
-from svd.data.dataset import WebVid10M
-from svd.training.loss import EDMLossDistributionLatent
+from diffusers.pipelines import StableVideoDiffusionPipeline
+from svd.data.dataset import WebVid10M,ImageDataset
+from svd.training.loss import EDMLossTimeNoise,EDMLossBaseline
 from svd.models.utils import pixel2latent, encode_image
-from svd.schedulers.edm import add_noise
-from svd.training.utils import load_checkpoint, init_dist, set_seed, save_videos_grid
+from svd.training.utils import load_checkpoint, init_dist, set_seed,load_PIL_images,save_videos_grid
 from svd.models.unet_spatio_temporal_condition import UNetSpatioTemporalConditionModel
 
 def main(
-        debug: False,
         use_ema: False,
         output_dir: str,
         pretrained_model_path: str,
         train_data: Dict,
+        validation_folder: str,
         motion_bucket_id: float=20.0,
         cfg_random_null_ratio: float = 0.1,
         resume_path: str = "",
         ema_decay: float = 0.9999,
         noise_scheduler_kwargs=None,
-        max_train_steps: int = 100,
+        max_train_steps: int = 10000,
+        validation_steps: int = 10000,
         learning_rate: float = 3e-5,
         scale_lr: bool = False,
         lr_scheduler: str = "constant",
@@ -48,9 +48,7 @@ def main(
         checkpointing_steps: int = -1,
         mixed_precision_training: bool = True,
         global_seed: int = 42,
-        max_grad_norm=1.0,
-        beta_m=15,
-        a=5
+        max_grad_norm: float = 1.0,
 ):
     *_, config = inspect.getargvalues(inspect.currentframe())
 
@@ -119,38 +117,64 @@ def main(
             load_checkpoint(unet, optimizer, lr_scheduler, ema_unet, filename=resume_path)
         else:
             load_checkpoint(unet, optimizer, lr_scheduler, filename=resume_path)
-    else:
-        global_step = 0
-        first_epoch = 0
+
+    global_step = 0
 
     # Enable gradient checkpointing
     if gradient_checkpointing:
         unet.enable_gradient_checkpointing()
 
-    unet = DDP(unet, device_ids=[local_rank], output_device=local_rank)
     # Get the training dataset
     train_dataset = WebVid10M(**train_data)
-    if not debug:
-        distributed_sampler = DistributedSampler(
-            train_dataset,
-            num_replicas=num_processes,
-            shuffle=True,
-            seed=global_seed
-        )
+    
+    distributed_sampler = DistributedSampler(
+        train_dataset,
+        num_replicas=num_processes,
+        shuffle=True,
+        seed=global_seed
+    )
 
-        # DataLoaders creation:
-        train_dataloader = torch.utils.data.DataLoader(
-            train_dataset,
-            batch_size=train_batch_size,
-            shuffle=False,
-            sampler=distributed_sampler,
-            num_workers=10,
-            pin_memory=True,
-            drop_last=True,
-        )
-    loss_fn = EDMLossDistributionLatent(P_mean=noise_scheduler_kwargs.P_mean, P_std=noise_scheduler_kwargs.P_std, \
-                                            sigma_data=noise_scheduler_kwargs.sigma_data,beta_m=beta_m,a=a)
-
+    # DataLoaders creation:
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=train_batch_size,
+        shuffle=False,
+        sampler=distributed_sampler,
+        num_workers=10,
+        pin_memory=True,
+        drop_last=True,
+    )
+    # get the test dataset
+    test_dataset = ImageDataset(validation_folder)
+    test_distributed_sampler = DistributedSampler(
+        test_dataset,
+        shuffle=False,
+    )
+    test_dataloader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=train_batch_size,
+        shuffle=False,
+        sampler=test_distributed_sampler,
+        num_workers=num_processes,
+        pin_memory=True,
+        drop_last=False,
+    )
+    validation_pipeline = StableVideoDiffusionPipeline.from_pretrained(
+        pretrained_model_path,
+        unet=unet,
+        image_encoder=image_encoder,
+        vae=vae).to(local_rank)
+    
+    # loss function for TimeNoise
+    loss_fn = EDMLossTimeNoise(P_mean=noise_scheduler_kwargs.P_mean, P_std=noise_scheduler_kwargs.P_std, \
+                                           sigma_data=noise_scheduler_kwargs.sigma_data,beta_m=noise_scheduler_kwargs.beta_m,a=noise_scheduler_kwargs.a)
+    
+    # baseline (train without TimeNoise): 
+    # loss_fn = EDMLossBaseline(P_mean=noise_scheduler_kwargs.P_mean, P_std=noise_scheduler_kwargs.P_std,sigma_data=noise_scheduler_kwargs.sigma_data)
+    
+    # DDP wrapper
+    unet = DDP(unet, device_ids=[local_rank], output_device=local_rank)
+    
     # Only show the progress bar once on each machine.
     progress_bar = tqdm(range(global_step, max_train_steps), disable=not is_main_process)
     progress_bar.set_description("Steps")
@@ -168,7 +192,6 @@ def main(
         # CLIP image embeddings
         encoder_hidden_states = encode_image(pixel_values[:, 0, :, :, :].float(), feature_extractor, image_encoder)
         # Predict the noise residual and compute loss
-        # Mixed-precision training
         loss = loss_fn(unet, latents, encoder_hidden_states, mixed_precision_training,pixel_values,vae,cfg_random_null_ratio,motion_bucket_id=motion_bucket_id,fps=train_data.fps)
         optimizer.zero_grad()
 
@@ -215,6 +238,35 @@ def main(
             torch.save(state_dict, os.path.join(save_path, f"checkpoint-step-{global_step}.ckpt"))
             logging.info(f"Saved state to {save_path} (global_step: {global_step})")
 
+        # Periodically validation
+        if global_step % validation_steps == 0:
+            unet.eval()
+            if use_ema:
+                ema_unet.store(unet.parameters())
+                ema_unet.copy_to(unet.parameters())
+
+            resolution = tuple(map(int, train_data.sample_size.split(',')))
+
+            for _, test_batch in enumerate(test_dataloader):
+                paths = test_batch['path']
+                names = test_batch['name']
+                images_list = load_PIL_images(paths, resolution) # resolution, need to check
+                generator = torch.manual_seed(42)
+                samples = validation_pipeline(images_list, output_type="pt", generator=generator, height=resolution[0],
+                                width=resolution[1],  num_frames=train_data.sample_n_frames, motion_bucket_id=motion_bucket_id).frames
+                samples = torch.stack(samples)
+
+                for sample, name in zip(samples, names):
+                    name = name.split('.')[0]
+                    save_path = f"{output_dir}/samples/{global_step}_ema_{use_ema}/{name}.mp4"
+                    sample = rearrange(sample, "t c h w -> c t h w").unsqueeze(dim=0)
+                    save_videos_grid(sample.cpu(), save_path, n_rows=1)
+                    print(f'the sample has been saved in {save_path}')
+
+            if use_ema:
+                ema_unet.restore(unet.parameters())
+
+            unet.train()
         logs = {"step_loss": loss.detach().item()}
         progress_bar.set_postfix(**logs)
 
